524526a19554c2ddf1c0a2bf8b40969f0bc4d5da
leonardo-lima-98 <leonardodelima98@outlook.com>
2025-07-28 22:11:01 -0300

    Deploy Test

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,10 +1,11 @@
 import multiprocessing
 
-max_requests = 1000
-max_requests_jitter = 50
+max_requests = 100
+max_requests_jitter = 5
 log_file = "-"
 bind = "0.0.0.0:8000"
-workers = (multiprocessing.cpu_count() * 2) + 1
+# workers = (multiprocessing.cpu_count() * 2) + 1
+workers = 4
 
 worker_class = "my_uvicorn_worker.MyUvicornWorker"
 
